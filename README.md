52e0a24649003537a4648b4128dc9dfa6d3174c0
Nathan Ryan <rockymountainshredder@gmail.com>
2022-04-03 15:34:02 -0600

    Updated README, added user input grid defintion functionality

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Etch A Sketch rendition as part of The Odin Project Curriculum
 Ideas to implement:
 
 [ ] - Choose color to paint etch-a-sketch with
-[ ] - Enable option to clear etch-a-sketch and input custom 
+[X] - Enable option to clear etch-a-sketch and input user generated number of squares per side
--- assets/scripts/testing.js
+++ assets/scripts/testing.js
@@ -1,5 +1,4 @@
 
-
 const grid = document.getElementById('grid-container');
 const reset = document.getElementById('reset');
 let columns = 16;
@@ -24,10 +23,39 @@ for (let i = 0; i < (columns * rows); i++) {
 
 // Create a function to reset the grid
 function resetGrid() {
-    const eraseCell = document.querySelectorAll('.grid-item');
-    for (let i = 0; i < eraseCell.length; i++) {
-        eraseCell[i].style.backgroundColor = 'lightgrey';
+
+    let userGrid = 0;
+    let root = document.querySelector(':root');
+
+    //Replace with user generated number of sides, ensure number is between 1 and 100
+    do {
+        userGrid = prompt('Enter in the number of squares per side you would like: ', 'Max # = 100');
+    } while (userGrid < 1 || userGrid > 100);
+
+    root.style.setProperty('--grid-columns', userGrid);
+    root.style.setProperty('--grid-rows', userGrid);
+    
+    // Delete every child in grid
+    grid.replaceChildren();
+
+    // Paint new user generated grid
+    for (let i = 0; i < (userGrid * userGrid); i++) {
+        const cell = document.createElement('div');
+        cell.style.backgroundColor = 'lightgrey';
+        cell.addEventListener('mouseenter', () => {
+            cell.style.backgroundColor = setBackgroundColor();
+            // cell.style.setProperty('filter', 'brightness(90%)');
+        });
+        // cell.addEventListener('mouseleave', () => {
+        //     cell.style.backgroundColor = 'white';
+        // });
+        grid.appendChild(cell).className = 'grid-item';
     }
+
+    // const eraseCell = document.querySelectorAll('.grid-item');
+    // for (let i = 0; i < eraseCell.length; i++) {
+    //     eraseCell[i].style.backgroundColor = 'lightgrey';
+    // }
 }
 
 // Random color generator courtesy of CSS-Tricks
